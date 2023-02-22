25c749df9a6252d31d0894dac314bfee14d99918
VaniaSasikirana <sk080501@gmail.com>
2023-02-22 21:47:27 +0700

    huhu

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'sql9599986' );
+define( 'DB_NAME', 'sql9600253' );
 
 /** Database username */
-define( 'DB_USER', 'sql9599986' );
+define( 'DB_USER', 'sql9600253' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'TC9SXgNvXt' );
+define( 'DB_PASSWORD', 'qjGjuYYzFQ' );
 
 /** Database hostname */
 define( 'DB_HOST', 'sql9.freemysqlhosting.net' );
